4fadc713822a597c1abcfecd6b1eae3f2b4e686f
lzvckaya@gmail.com <lzvckaya@gmail.com>
2024-06-19 00:53:45 +0900

    listen socket 기본 구현 및 client 설정

--- Dummy_Client/Program.cs
+++ Dummy_Client/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace Dummy_Client
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Dummy_Client
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Client");
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+            Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                socket.Connect(endPoint);
+                Console.WriteLine($"COnnected To {socket.RemoteEndPoint.ToString()}");
+
+                byte[] sendBuff = Encoding.UTF8.GetBytes("Connect Server");
+                int sendBytes = socket.Send(sendBuff);
+
+                byte[] recvBuff = new byte[1024];
+                int recvBytes = socket.Receive(recvBuff);
+                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+
+                Console.WriteLine($"[From Server]{recvData}");
+
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+
+
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+ 
+
         }
     }
 }
--- Tcp_Server_Core/Program.cs
+++ Tcp_Server_Core/Program.cs
@@ -1,11 +1,53 @@
-﻿namespace Tcp_Server_Core
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Tcp_Server_Core
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
-            Console.WriteLine("Server");
+            Socket listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                listenSocket.Bind(endPoint);
+
+                listenSocket.Listen(10);
+
+                while (true)
+                {
+
+
+                    Console.WriteLine("Listening....");
+
+                    Socket clientSocket = listenSocket.Accept();
+
+                    byte[] recvBuff = new byte[1024];
+                    int recvBytes = clientSocket.Receive(recvBuff);
+                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                    Console.WriteLine($"[From Client] {recvData}");
+
+                    byte[] sendBuff = Encoding.UTF8.GetBytes("Connect Server!");
+                    clientSocket.Send(sendBuff);
+
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
         }
     }
 }
