295825ec4524405b2cce478f96641ba4147beca8
lzvckaya@gmail.com <lzvckaya@gmail.com>
2024-06-19 01:13:37 +0900

    AcceptAsync를 이용한 non-blocking

--- Tcp_Server_Core/Listener.cs
+++ Tcp_Server_Core/Listener.cs
@@ -11,19 +11,55 @@ namespace Tcp_Server_Core
     class Listener
     {
         Socket _listenSocket;
+        Action<Socket> _onAcceptHandler;
 
-        public void Init(IPEndPoint endPoint)
+        public void Init(IPEndPoint endPoint,Action<Socket> onAcceptHandler)
         {
 
             _listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            _onAcceptHandler += onAcceptHandler;
+
             _listenSocket.Bind(endPoint);
             _listenSocket.Listen(10);
 
+            SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+            args.Completed += new EventHandler<SocketAsyncEventArgs>(OnAcceptCompleted);   
+            RegisterAccept(args);   
+        }
+
+        void RegisterAccept(SocketAsyncEventArgs args)
+        {
+            args.AcceptSocket = null;
+
+            bool pending = _listenSocket.AcceptAsync(args);
+            if (!pending)
+            {
+                OnAcceptCompleted(null, args);
+            }
+        }
+
+        void OnAcceptCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            try
+            {
+                if (args.SocketError == SocketError.Success)
+                {
+                    _onAcceptHandler.Invoke(args.AcceptSocket);
+                }
+                else
+                    Console.WriteLine(args.SocketError.ToString());
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
 
         public Socket Accept()
         {
-            return _listenSocket.Accept();
+
+             return _listenSocket.Accept();
         }
 
     }
--- Tcp_Server_Core/Program.cs
+++ Tcp_Server_Core/Program.cs
@@ -8,39 +8,44 @@ namespace Tcp_Server_Core
     {
         static Listener _listener = new Listener();
 
+        static void OnAcceptHandler(Socket clientSocket)
+        {
+            try
+            {
+                byte[] recvBuff = new byte[1024];
+                int recvBytes = clientSocket.Receive(recvBuff);
+                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                Console.WriteLine($"[From Client] {recvData}");
+
+                byte[] sendBuff = Encoding.UTF8.GetBytes("Connect Server!");
+                clientSocket.Send(sendBuff);
+
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+
         static void Main(string[] args)
         {
             string host = Dns.GetHostName();
             IPHostEntry ipHost = Dns.GetHostEntry(host);
             IPAddress ipAddr = ipHost.AddressList[0];
             IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
-
             
-            try
+            _listener.Init(endPoint, OnAcceptHandler);
+            Console.WriteLine("Listening...");
+
+
+
+            while (true)
             {
-                _listener.Init(endPoint);
 
-                while (true)
-                {
-                    Console.WriteLine("Listening....");
-                    Socket clientSocket = _listener.Accept();
-
-                    byte[] recvBuff = new byte[1024];
-                    int recvBytes = clientSocket.Receive(recvBuff);
-                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
-                    Console.WriteLine($"[From Client] {recvData}");
-
-                    byte[] sendBuff = Encoding.UTF8.GetBytes("Connect Server!");
-                    clientSocket.Send(sendBuff);
-
-                    clientSocket.Shutdown(SocketShutdown.Both);
-                    clientSocket.Close();
-
-                }
-            }
-            catch (SocketException e)
-            {
-                Console.WriteLine(e.Message);
             }
 
 
